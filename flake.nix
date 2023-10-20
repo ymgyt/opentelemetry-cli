ffba78aa5d2f1271a29d9e911028af8a28b1c589
ymgyt <yamaguchi7073xtt@gmail.com>
2023-10-21 07:43:05 +0900

    chore: update nix config

--- flake.nix
+++ flake.nix
@@ -1,40 +1,50 @@
 {
-  description = "A very basic flake";
+  description = "opentelemetry-cli flake";
 
-  inputs.std.url = "github:divnix/std";
-  inputs.std.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  inputs.fenix.url = "github:nix-community/fenix";
-  inputs.crane.url = "github:ipetkov/crane";
-  inputs.crane.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.crane.inputs.flake-utils.follows = "std/flake-utils";
-
-  inputs.nixpkgs.follows = "fenix/nixpkgs";
-
-  outputs = inputs @ {
-    self,
-    std,
-    ...
-  }:
-    std.growOn {
-      inherit inputs;
-      systems = ["x86_64-linux"];
-      cellsFrom = ./nix;
-      cellBlocks = [
-        (std.devshells "devshells")
-        (std.installables "packages")
-      ];
-    } {
-      devShells = std.harvest self ["automation" "devshells"];
-      packages = std.harvest self ["hello" "packages"];
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
     };
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
+
+  outputs = { self, nixpkgs, flake-utils, crane, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        # Tell crane Cargo.toml path to inspect package name and version
+        crate = craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; };
+
+        opentelemetry-cli = craneLib.buildPackage {
+          inherit (crate) pname version;
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+          buildInputs = [ ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ ];
+        }; 
+      in {
+        packages.default = self.packages."${system}".telemetryd;
+        packages.opentelemetry-cli = opentelemetry-cli;
+      });
 }
